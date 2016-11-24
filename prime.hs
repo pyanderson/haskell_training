7cba2236b4c3ce7a19d32d2a3d30a92b1577d60a
Anderson <anderson.sl93@hotmail.com>
2016-11-24 13:02:55 -0300

    change pipe

--- prime.hs
+++ prime.hs
@@ -6,10 +6,12 @@ ePrimo n			= n==2 || n>2 && all ((> 0).rem n) (2:[3,5..floor.sqrt.fromIntegral $
 -- divisores
 ePrimo'Aux			:: Int -> [Int] -> Bool
 ePrimo'Aux _ [] 		= True
-ePrimo'Aux n (x:xs) 	| n `mod` x == 0 = False
-  | otherwise = ePrimo'Aux n xs
+ePrimo'Aux n (x:xs)
+  | n `mod` x == 0 		= False
+  | otherwise 			= ePrimo'Aux n xs
 
 ePrimo'				:: Int -> Bool
-ePrimo' n			| n == 2 = True
-  | n > 2 = ePrimo'Aux n (2:[3,5..floor.sqrt.fromIntegral $ n + 1])
-  | otherwise = False
+ePrimo' n
+  | n == 2 				= True
+  | n > 2 				= ePrimo'Aux n (2:[3,5..floor.sqrt.fromIntegral $ n + 1])
+  | otherwise 			= False
