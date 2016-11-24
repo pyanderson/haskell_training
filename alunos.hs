26c60ddcc1b3423416fc32dab08789773f052b76
Anderson <anderson.sl93@hotmail.com>
2016-11-24 15:05:57 -0300

    ident

--- alunos.hs
+++ alunos.hs
@@ -1,14 +1,14 @@
-data Nota 						= Nota Float Float Float deriving (Show)
-data Aluno 						= Aluno [Char] Nota deriving (Show)
+data Nota 					= Nota Float Float Float deriving (Show)
+data Aluno 					= Aluno [Char] Nota deriving (Show)
 
-media 							:: Aluno -> Float
-media (Aluno _ (Nota n1 n2 n3)) 			= (n1 + n2 + n3) / 3
+media 						:: Aluno -> Float
+media (Aluno _ (Nota n1 n2 n3)) 		= (n1 + n2 + n3) / 3
 
-ordena 							:: [Aluno] -> [Aluno]
-ordena [] 						= []
-ordena ((Aluno n nn):xs) 				= [(Aluno m nm) | (Aluno m nm) <- xs, m <= n]
-							++ [(Aluno n nn)] ++
-							[(Aluno m nm) | (Aluno m nm) <- xs, m > n]
+ordena 						:: [Aluno] -> [Aluno]
+ordena [] 					= []
+ordena ((Aluno n nn):xs) 			= [(Aluno m nm) | (Aluno m nm) <- xs, m <= n]
+						++ [(Aluno n nn)] ++
+						[(Aluno m nm) | (Aluno m nm) <- xs, m > n]
 
-ordenaMedia 						:: [Aluno] -> [([Char], Float)]
-ordenaMedia xs 						= [(n, media (Aluno n nn)) | (Aluno n nn) <- ordena xs]
+ordenaMedia 					:: [Aluno] -> [([Char], Float)]
+ordenaMedia xs 					= [(n, media (Aluno n nn)) | (Aluno n nn) <- ordena xs]
