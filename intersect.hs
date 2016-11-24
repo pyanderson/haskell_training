bf3ac05119cf553d938a3b9d45a5f340c4e14226
Anderson <anderson.sl93@hotmail.com>
2016-11-24 14:33:38 -0300

    fix intersect

--- intersect.hs
+++ intersect.hs
@@ -4,7 +4,7 @@ find 			:: (Ord t) => t -> [t] -> Bool
 find _ [] 		= False
 find n (x:xs)
   | n == x 		= True
-  | otherwise 	= find n xs
+  | otherwise 		= find n xs
 
 intersect 		:: (Ord t) => [t] -> [t] -> [t]
-intersect xs ys = [x | x <- xs, find x ys]
+intersect xs ys 	= [x | x <- xs, find x ys]
