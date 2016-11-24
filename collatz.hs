a572b4bb6a0d501461cdda83b78dbffd08c88883
Anderson <anderson.sl93@hotmail.com>
2016-11-24 13:53:56 -0300

    fix collatz

--- collatz.hs
+++ collatz.hs
@@ -2,13 +2,13 @@ collatz				:: Int -> [Int]
 collatz 1			= [1]
 collatz n
   | odd n			= n:(collatz ((n * 3) + 1))
-  | otherwise		= n:(collatz (div n 2))
+  | otherwise			= n:(collatz (div n 2))
 
 tamCollatz			:: Int -> Int
 tamCollatz			= length . collatz
 
 allCollatz			:: [Int] -> [[Int]]
-allCollatz xs		= [collatz x | x <- xs]
+allCollatz xs			= [collatz x | x <- xs]
 
-allTamCollatz		:: [Int] -> [(Int, Int)]
-allTamCollatz xs	= [(x, tamCollatz x) | x <- xs]
+allTamCollatz			:: [Int] -> [(Int, Int)]
+allTamCollatz xs		= [(x, tamCollatz x) | x <- xs]
