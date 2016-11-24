9e83a28a8958332077862dd7aca64b74ba07d511
Anderson <anderson.sl93@hotmail.com>
2016-11-24 14:09:19 -0300

    fix transpose

--- transpose.hs
+++ transpose.hs
@@ -1,5 +1,6 @@
--- Dentro de Data.List já existe uma função chamada transpose
-transpose				:: [[Char]] -> [[Char]]
+-- Dentro de Data.List já existe uma função chamada transpose, porém não
+-- é carregada no Prelude
+transpose			:: [[Char]] -> [[Char]]
 transpose []			= []
 transpose ([]:xss) 		= transpose xss
-transpose ((x:xs):xss) 	= (x:[h | (h:_) <- xss]) : transpose (xs:[t | (_:t) <- xss])
+transpose ((x:xs):xss) 		= (x:[h | (h:_) <- xss]) : transpose (xs:[t | (_:t) <- xss])
